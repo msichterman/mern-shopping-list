85cc92c8e25173e09fe69040863224bef6117ef2
Matthew Sichterman <msichterman1@gmail.com>
2019-08-18 16:46:22 -0500

    store.js added to allow cross browser compatibility

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,10 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+const enhancer = composeEhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
