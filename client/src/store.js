2222908a5dfa5d18de1f8c4e92b10a7ad2ba7e1e
Matthew Sichterman <msichterman1@gmail.com>
2019-09-08 22:04:21 -0500

    Sign up and logout added along with authentication.

--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,13 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleWare = [thunk];
 
-const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
-
-const enhancer = composeEnhancers(applyMiddleware(...middleware));
-
-const store = createStore(rootReducer, initialState, enhancer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleWare))
+);
 
 export default store;
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://msichy:ABCabc123@mern-shopping-xthmh.mongodb.net/test?retryWrites=true&w=majority'
-}
